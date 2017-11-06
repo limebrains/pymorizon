cdb8fc819a8194fa6d8a4e4db5cdfe5f252d3a88
michal <michal@iMac-lb-4.local>
2017-11-06 13:36:23 +0100

    start

---
+++ morizon/__init__.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+version = '0.0.3'
+
+VERSION = tuple(map(int, version.split('.')))
+__version__ = VERSION
+__versionstr__ = version
+
+if (2, 7) <= sys.version_info < (3, 6):
+    import logging
+
+    DEBUG = os.environ.get('DEBUG')
+
+    logger = logging.getLogger('olx')
+    logging.basicConfig(level=logging.DEBUG)
+
+BASE_URL = 'https://www.morizon.pl'
---
+++ morizon/category.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import json
+import logging
+log = logging.getLogger(__file__)
+logging.basicConfig(level=logging.DEBUG)
+
